f848092747a37c68d089af7415f56d527ecd4535
hnavudur <132383327+hnavudur@users.noreply.github.com>
2023-05-09 08:20:15 +0530

    InitialProjectSetup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RedundantMultithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
